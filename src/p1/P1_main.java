224fc90aec94698808d96035c49c9aeeb765c8ee
fzapp <fzapp@smail.uni-loeln.de>
2025-11-19 12:27:57 +0100

    aenderung_Einfuehrung_Aufgabenblatt5_Aufg1

---
+++ src/p1/P1_main.java
@@ -0,0 +1,26 @@
+package p1;
+
+public class P1_main {
+
+	public static void main(String[] args) {
+		int x=0;
+		while (x <= 10 ) {
+			x = x+1;
+			System.out.println(x);
+		}
+		x = 0;
+		// TODO Auto-generated method stub
+		for (int i = x+1; i <= 11; i++) {
+			System.out.println(i);
+
+	}
+		x = 0;
+		do {
+			x = x+1;
+			System.out.println(x);
+		}
+		while(x <=10);
+	
+	}
+
+}
